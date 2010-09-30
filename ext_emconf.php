2258142a918c06ec0f73eb491fac71fdd1e1e902
presedo <presedo@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-30 08:08:35 +0000

    version 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/googlequery/trunk@38769 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "googlequery".
 #
-# Auto generated 27-09-2010 17:50
+# Auto generated 30-09-2010 10:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'expressions' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"964c";s:10:"README.txt";s:4:"d0d0";s:24:"class.tx_googlequery.php";s:4:"6663";s:25:"class.tx_googlequery2.php";s:4:"e79f";s:30:"class.tx_googlequery_cache.php";s:4:"134c";s:31:"class.tx_googlequery_parser.php";s:4:"351a";s:21:"ext_conf_template.txt";s:4:"f2bd";s:12:"ext_icon.gif";s:4:"9eb4";s:14:"ext_icon_2.gif";s:4:"afaf";s:17:"ext_localconf.php";s:4:"7ed1";s:14:"ext_tables.php";s:4:"e1d8";s:14:"ext_tables.sql";s:4:"3862";s:16:"locallang_db.xml";s:4:"7d1d";s:7:"tca.php";s:4:"134f";s:20:"tca_googlequery2.php";s:4:"adc2";s:14:"doc/manual.pdf";s:4:"d69c";s:14:"doc/manual.sxw";s:4:"a18b";s:36:"res/icons/add_googlequery_wizard.gif";s:4:"d69c";s:41:"res/icons/icon_tx_googlequery_queries.gif";s:4:"d291";s:42:"res/icons/icon_tx_googlequery_queries2.gif";s:4:"afaf";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"964c";s:10:"README.txt";s:4:"d0d0";s:24:"class.tx_googlequery.php";s:4:"e498";s:25:"class.tx_googlequery2.php";s:4:"e79f";s:30:"class.tx_googlequery_cache.php";s:4:"134c";s:31:"class.tx_googlequery_parser.php";s:4:"351a";s:21:"ext_conf_template.txt";s:4:"f2bd";s:12:"ext_icon.gif";s:4:"9eb4";s:14:"ext_icon_2.gif";s:4:"afaf";s:17:"ext_localconf.php";s:4:"7ed1";s:14:"ext_tables.php";s:4:"e1d8";s:14:"ext_tables.sql";s:4:"3862";s:16:"locallang_db.xml";s:4:"7d1d";s:7:"tca.php";s:4:"134f";s:20:"tca_googlequery2.php";s:4:"adc2";s:14:"doc/manual.pdf";s:4:"d69c";s:14:"doc/manual.sxw";s:4:"a18b";s:36:"res/icons/add_googlequery_wizard.gif";s:4:"d69c";s:41:"res/icons/icon_tx_googlequery_queries.gif";s:4:"d291";s:42:"res/icons/icon_tx_googlequery_queries2.gif";s:4:"afaf";}',
 	'suggests' => array(
 	),
 );
